f1157f99223326beff098b2335780e91dd15f7c0
Richard DeSilvey <rdesilvey@gmail.com>
2016-04-12 12:10:11 -0600

    Removed the 'authenticate' method from the Routes file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,6 @@ Route::post('/login', function(){
     
     if (Auth::attempt(Input::only('email', 'password'))){
         Authen::grant();
-        Authen::authenticate();
         return Authen::check() == 1 ? "true":"false";
     }else{
         return "invalid";
@@ -23,6 +22,5 @@ Route::post('/login', function(){
 });
 
 Route::get('/auth', function(){
-    Authen::authenticate();
     return Authen::check() == 1 ? "true":"false";
 });
