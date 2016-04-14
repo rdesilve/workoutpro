2335bbc42bcef18742c06bc42b194f80e60c4774
Richard DeSilvey <rdesilvey@gmail.com>
2016-04-14 10:06:13 -0600

    Simplified the class

--- app/Authen.php
+++ app/Authen.php
@@ -13,47 +13,17 @@ define('AUTHFLAG', 'loggedin');
 class Authen {
     
     
-    /**
-     * Flags the app to log out the user when they are redirected
-     * to another page.
-     */
-    public static function logout(){
-        unset($_SESSION[AUTHFLAG]);
-        unset($_SESSION[USERDATA]);
-        session_destroy();
+    public static function check() {
+        return (isset($_SESSION[AUTHFLAG]) && $_SESSION[AUTHFLAG]);
     }
-   
-    
-    /**
-     * Checks to see if the user is logged in.
-     * @return type True if the user is logged in, false otherwise
-     */
-    public static function check(){
-        if (isset($_SESSION[AUTHFLAG])){
-            return $_SESSION[AUTHFLAG];
-        }else{
-            return false;
-        }
-    }
-    /**
-     * Retrives the user's name
-     * @return type The name of the user currently logged in, null otherwise
-     */
-    public static function data(){
-        if (isset($_SESSION[USERDATA])){
-            return $_SESSION[USERDATA];
-        }else{
-            return null;
-        }
-    }
-    
-    /**
-     * Logs the user in, or grants access to restricted pages.
-     * @param type $data The name of the user.
-     */
-    public static function grant($data = "undefined"){
+
+    public static function grant($data = 'undefined') {
         $_SESSION[AUTHFLAG] = true;
-        $_SESSION[USERDATA] = $data;
+        $_SESSION[DATA] = $data;
+    }
+
+    public static function loggout() {
+        session_destroy();
     }
 
 }
